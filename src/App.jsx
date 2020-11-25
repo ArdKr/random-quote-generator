9c3a7a7d6620ea857467acfa8610b2f3db2f6ccf
ArdKr <mkras01@gmx.de>
2020-11-25 20:49:15 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// Import Views
+import Home from "./views/Home";
+import Author from "./views/Author";
 
 const App = () => {
-  return <div>test</div>;
+  return (
+    <Router>
+      {/* This part is only for testing for now */}
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/author/John">Author</Link>
+          </li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/author/:name">
+          <Author />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
